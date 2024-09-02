b74b95121d11adef4d002c454b468f92f68be332
huggus1629 <hugo@belart.net>
2024-09-02 22:02:43 +0200

    Test #3

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from direct.showbase.ShowBase import ShowBase
 
 def main():
     print("hello world")
-    # test message from pc1
+    # test message from pc
     # test message from laptop
+    # test message 2 from pc
 
 
 if __name__ == '__main__':
