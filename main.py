03f53ad74ec69bc41cd320cfe92ec608c70622a4
huggus1629 <hugo@belart.net>
2024-09-03 08:18:34 +0200

    Example grassy scenery program

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
+from math import pi, sin, cos
+
 from direct.showbase.ShowBase import ShowBase
+from direct.task import Task
 
 
-def main():
-    print("hello world")
-    # test message from pc
-    # test message from laptop
-    # test message 2 from pc
+class MyApp(ShowBase):
+    def __init__(self):
+        ShowBase.__init__(self)
+
+        # Load the environment model.
+        self.scene = self.loader.loadModel("models/environment")
+
+        # Reparent the model to render.
+        self.scene.reparentTo(self.render)
+
+        # Apply scale and position transforms on the model.
+        self.scene.setScale(0.25, 0.25, 0.25)
+        self.scene.setPos(-8, 42, 0)
+        # Add the spinCameraTask procedure to the task manager.
+
+        self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
+
+    # Define a procedure to move the camera.
+
+    def spinCameraTask(self, task):
+        angleDegrees = task.time * 6.0
+        angleRadians = angleDegrees * (pi / 180.0)
+        self.camera.setPos(20 * sin(angleRadians), -20 * cos(angleRadians), 3)
+        self.camera.setHpr(angleDegrees, 0, 0)
+        return Task.cont
 
 
-if __name__ == '__main__':
-    main()
+app = MyApp()
+app.run()
