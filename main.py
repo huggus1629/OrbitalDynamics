c76980c00393c82780603f3e6738a5041530f89e
huggus1629 <hugo@belart.net>
2024-09-03 08:23:38 +0200

    Replaced spaces w/ tabs

--- main.py
+++ main.py
@@ -5,30 +5,30 @@ from direct.task import Task
 
 
 class MyApp(ShowBase):
-    def __init__(self):
-        ShowBase.__init__(self)
+	def __init__(self):
+		ShowBase.__init__(self)
 
-        # Load the environment model.
-        self.scene = self.loader.loadModel("models/environment")
+		# Load the environment model.
+		self.scene = self.loader.loadModel("models/environment")
 
-        # Reparent the model to render.
-        self.scene.reparentTo(self.render)
+		# Reparent the model to render.
+		self.scene.reparentTo(self.render)
 
-        # Apply scale and position transforms on the model.
-        self.scene.setScale(0.25, 0.25, 0.25)
-        self.scene.setPos(-8, 42, 0)
-        # Add the spinCameraTask procedure to the task manager.
+		# Apply scale and position transforms on the model.
+		self.scene.setScale(0.25, 0.25, 0.25)
+		self.scene.setPos(-8, 42, 0)
+		# Add the spinCameraTask procedure to the task manager.
 
-        self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
+		self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
 
-    # Define a procedure to move the camera.
+	# Define a procedure to move the camera.
 
-    def spinCameraTask(self, task):
-        angleDegrees = task.time * 6.0
-        angleRadians = angleDegrees * (pi / 180.0)
-        self.camera.setPos(20 * sin(angleRadians), -20 * cos(angleRadians), 3)
-        self.camera.setHpr(angleDegrees, 0, 0)
-        return Task.cont
+	def spinCameraTask(self, task):
+		angleDegrees = task.time * 6.0
+		angleRadians = angleDegrees * (pi / 180.0)
+		self.camera.setPos(20 * sin(angleRadians), -20 * cos(angleRadians), 3)
+		self.camera.setHpr(angleDegrees, 0, 0)
+		return Task.cont
 
 
 app = MyApp()
