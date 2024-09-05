bc737f3b184cdb7c4ff4b73994877086b4ae5b19
huggus1629 <hugo@belart.net>
2024-09-05 23:47:11 +0200

    Loads cubemap for the skybox (not working yet)

--- main.py
+++ main.py
@@ -20,26 +20,19 @@ class MyApp(ShowBase):
 	def __init__(self):
 		ShowBase.__init__(self)
 
-		# Load the environment model.
-		self.scene = self.loader.loadModel("models/environment")
-
-		# Reparent the model to render.
-		self.scene.reparentTo(self.render)
-
-		# Apply scale and position transforms on the model.
-		self.scene.setScale(0.25, 0.25, 0.25)
-		self.scene.setPos(-8, 42, 0)
-		# Add the spinCameraTask procedure to the task manager.
-
-		self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
+		self.skybox_texture = self.loader.loadCubeMap('skybox/cubemap_#.png')
+		self.skybox = self.loader.loadModel('models/box')
+		self.skybox.setScale(2)
+		self.skybox.reparentTo(self.render)
+		self.skybox.setTexture(self.skybox_texture, 1)
+		# self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
 
 	# Define a procedure to move the camera.
-
 	def spinCameraTask(self, task):
-		angleDegrees = task.time * 6.0
+		angleDegrees = task.time * 24.0
 		angleRadians = angleDegrees * (pi / 180.0)
 		self.camera.setPos(20 * sin(angleRadians), -20 * cos(angleRadians), 3)
-		self.camera.setHpr(angleDegrees, 20 * sin(5 * angleRadians), 0)
+		self.camera.setHpr(angleDegrees, 0, 0)
 		return Task.cont
 
 
