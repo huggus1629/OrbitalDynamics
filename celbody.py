d3c5bd80e029757e39a4525249ef144ed2cf86e0
huggus1629 <hugo@belart.net>
2024-11-13 13:59:28 +0100

    Added physical properties

--- celbody.py
+++ celbody.py
@@ -4,7 +4,7 @@ from panda3d.core import NodePath, ModelNode
 
 
 class CelBody:
-	def __init__(self, base, name, model_path):
+	def __init__(self, base, name, model_path, mass, vec3_velocity):
 		self.node = NodePath(ModelNode(name))  # creates a ModelNode and wraps it in a NodePath
 		self.name = name
 
@@ -13,3 +13,9 @@ class CelBody:
 		# load the planet model and attach it to the NodePath
 		self.model = self.loader.loadModel(model_path)
 		self.model.reparentTo(self.node)
+
+		# given physical properties
+		self.mass = mass
+		self.vec3_velocity = vec3_velocity
+
+		self.frame_force = 0
