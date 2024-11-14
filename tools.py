58ffd5114283a57e974788d4e9a23bad31c57b27
huggus1629 <hugo@belart.net>
2024-11-14 10:57:50 +0100

    Added function to calculate vector sum

--- tools.py
+++ tools.py
@@ -2,5 +2,19 @@ def digits_after_decimal(x):
 	return len(str(float(x)).split('.')[1])
 
 
+# flips sign of vector
 def vec_neg(vec: tuple):
 	return tuple(- comp for comp in vec)
+
+
+# sums up a list of vectors
+def vec_sum(l_vec: list[tuple]):
+	dim = len(l_vec[0])
+	fres = [0 for _ in range(dim)]
+	for vec in l_vec:
+		if len(vec) != dim:
+			return
+		for i in range(dim):
+			fres[i] += vec[i]
+
+	return tuple(fres)
