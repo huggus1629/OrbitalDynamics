c0f44866d09b90c2b1d185dc65a097f61e51dcf8
huggus1629 <hugo@belart.net>
2024-11-14 11:41:34 +0100

    Removed return value type hints

--- tools.py
+++ tools.py
@@ -3,12 +3,12 @@ def digits_after_decimal(x):
 
 
 # flips sign of vector
-def vec_neg(vec: tuple[float]) -> tuple[float, ...]:
+def vec_neg(vec: tuple[float]):
 	return tuple(- comp for comp in vec)
 
 
 # sums up a list of vectors
-def vec_sum(l_vec: list[tuple[float]]) -> tuple[float, ...] | None:
+def vec_sum(l_vec: list[tuple[float]]):
 	dim = len(l_vec[0])
 	fres = [0.0 for _ in range(dim)]
 	for vec in l_vec:
@@ -21,5 +21,5 @@ def vec_sum(l_vec: list[tuple[float]]) -> tuple[float, ...] | None:
 
 
 # multiplies vector by scalar
-def vec_mul(vec: tuple[float], s) -> tuple[float, ...]:
+def vec_mul(vec: tuple[float], s):
 	return tuple(s * comp for comp in vec)
