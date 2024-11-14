11d83bb5ebd7f0f01a5518cad755cb70f3ac69ee
huggus1629 <hugo@belart.net>
2024-11-14 11:34:56 +0100

    Added type hints

--- tools.py
+++ tools.py
@@ -3,14 +3,14 @@ def digits_after_decimal(x):
 
 
 # flips sign of vector
-def vec_neg(vec: tuple):
+def vec_neg(vec: tuple[float]) -> tuple[float, ...]:
 	return tuple(- comp for comp in vec)
 
 
 # sums up a list of vectors
-def vec_sum(l_vec: list[tuple]):
+def vec_sum(l_vec: list[tuple[float]]) -> tuple[float, ...] | None:
 	dim = len(l_vec[0])
-	fres = [0 for _ in range(dim)]
+	fres = [0.0 for _ in range(dim)]
 	for vec in l_vec:
 		if len(vec) != dim:
 			return
