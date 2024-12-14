c4d09bab8352b3c4dd9a1086633f3c965825caf1
huggus1629 <hugo@belart.net>
2024-12-14 14:51:12 +0100

    Pass CelBody initial pos as argument to __init__

--- celbody.py
+++ celbody.py
@@ -6,7 +6,7 @@ import math
 
 
 class CelBody:
-	def __init__(self, base, name, model_path, mass, vec3_velocity: tuple[float, ...]):
+	def __init__(self, base, name, model_path, init_pos, mass, vec3_velocity: tuple[float, ...]):
 		self.node = NodePath(ModelNode(name))  # creates a ModelNode and wraps it in a NodePath
 		self.name = name
 
@@ -15,6 +15,8 @@ class CelBody:
 		# load the planet model and attach it to the NodePath
 		self.model = self.loader.loadModel(model_path)
 		self.model.reparentTo(self.node)
+
+		self.node.setPos(init_pos)
 
 		# given physical properties
 		self.mass = mass
